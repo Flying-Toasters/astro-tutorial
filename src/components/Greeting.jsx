fca4a7eac19c86b6e9a7d9009c6ab28c67ab3f29
Rodrigo Domingos <rodrigosantinid@gmail.com>
2026-01-16 15:55:08 +0000

    fixed greeting button

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'preact/hooks';
 
 export default function Greeting({messages}) {
-
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+      <button 
+        onClick={() => setGreeting(randomMessage())}
+        class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+      >
         New Greeting
       </button>
     </div>
